e75e4ea2ed17a60ac7762b761acc6229fd928651
DonDon <dondonfurry@gmail.com>
2026-02-10 16:20:02 +0900

    fix 0210 1

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'DonDon\'s Portfolio',
       home: PortfolioPage(),
     );
   }
